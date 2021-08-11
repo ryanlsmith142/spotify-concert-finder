0a86046548f837b671dfe45a4aed0edce9804c32
Ryan Smith <ryan.l.smith142@gmail.com>
2021-08-10 20:28:02 -0500

    Attempting to connect react and go

--- backend/main.go
+++ backend/main.go
@@ -48,6 +48,8 @@ func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Simple Server")
 	})
+
+	http.HandleFunc("/ws", serveWs)
 }
 func main() {
 	fmt.Println("Chat App v1")
